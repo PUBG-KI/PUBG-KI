36b4c3a09e401641e70a58c9e56bd5f7955a79ab
DESKTOP-PHOR79V\KGA <wodbs4889@gmail.com>
2025-01-13 13:09:06 +0900

    Weapon Asset / 기본 틀

---
+++ Source/PUBG/Public/Weapon/Guns/Gun_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon_Base.h"
+#include "Gun_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUBG_API AGun_Base : public AWeapon_Base
+{
+	GENERATED_BODY()
+	
+};
