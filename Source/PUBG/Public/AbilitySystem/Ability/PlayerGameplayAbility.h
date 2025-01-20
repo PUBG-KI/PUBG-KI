6b7ac653d8f875181bb7332fe35121efc4583771
JaeYun <wodbs4889@gmail.com>
2025-01-20 10:05:25 +0900

    오류 코드 수정

--- Source/PUBG/Public/AbilitySystem/Ability/PlayerGameplayAbility.h
+++ Source/PUBG/Public/AbilitySystem/Ability/PlayerGameplayAbility.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/Ability/BaseGameplayAbility.h"
-#include "Weapon/Guns/Gun_Base.h"
+//#include "Weapon/Guns/Gun_Base.h"
 #include "PlayerGameplayAbility.generated.h"
 
 /**
@@ -22,10 +22,7 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Ability")
 	ABasePlayerController* GetPlayerControllerFromActorInfo();
 
-	//UFUNCTION(BlueprintPure, category = "Ability")
-	//bool GetPlayerEquipedWeapon();
 
-	
 private:
 	TWeakObjectPtr<APlayerCharacter> CachedPlayerCharacter;
 	TWeakObjectPtr<ABasePlayerController> CachedPlayerController;
