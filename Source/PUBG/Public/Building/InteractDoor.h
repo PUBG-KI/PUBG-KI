606afa02d9a32fa7136795cf227c0beaae08ead9
DESKTOP-HAOF24A\user <yea2979@naver.com>
2025-01-22 21:47:06 +0900

    ShantyTown 에셋 추가 , 문 상호작용

---
+++ Source/PUBG/Public/Building/InteractDoor.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractInterface.h"
+#include "Components/TimelineComponent.h"
+
+#include "InteractDoor.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PUBG_API AInteractDoor : public AActor , public IInteractInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractDoor();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite ,meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* DoorMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* DoorCollisionBox;
+
+	virtual void InteractWith_Implementation(APlayerCharacter* Character) override;
+	virtual FText LookAt() override; 
+
+	bool IsOpen = false;
+
+	
+
+protected:
+	
+	FTimeline TimeLine;
+	UPROPERTY(EditAnywhere)
+	UCurveFloat* CurveFloat;
+
+	UPROPERTY(EditAnywhere)
+	float DoorRotateAngle = 90.0f;
+
+	UFUNCTION()
+	void OpenDoor(float Value); 
+	
+};
