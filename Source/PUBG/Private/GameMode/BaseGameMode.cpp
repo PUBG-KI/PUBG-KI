fce954e1afa17e962919357435e68e0ba237d0a2
naioooo <34563332+naioooo@users.noreply.github.com>
2025-02-24 15:40:04 +0900

    게임스타트 변수 추가

--- Source/PUBG/Private/GameMode/BaseGameMode.cpp
+++ Source/PUBG/Private/GameMode/BaseGameMode.cpp
@@ -33,7 +33,7 @@ void ABaseGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
-	if (HasAuthority()) // 서버에서만 실행
+	if (HasAuthority() && GameStart) // 서버에서만 실행
 	{
 		ABaseGameState* GS = Cast<ABaseGameState>(GameState);
 		if (GS && !GS->GetIsGameStarted())
@@ -45,12 +45,6 @@ void ABaseGameMode::PostLogin(APlayerController* NewPlayer)
 				// 게임 시작 타이머 실행
 				GI->GetTimeManager()->StartGameTimer();
 			}
-
-			// if (GI)
-			// {
-			// 	// 게임 시작 타이머 실행
-			// 	GI->GetTimeManager()->StartGameTimer();
-			// }
 		}	
 	}
 
